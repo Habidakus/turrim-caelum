1fea531f12ccbd4dc07fcc364b89c1aa405a9f00
Jason Abbott <woebane@gmail.com>
2024-04-04 09:43:18 -0700

    Rocks have slightly different color variations

--- Scripts/rock.gd
+++ Scripts/rock.gd
@@ -1,5 +1,11 @@
 extends Area2D
 
+func _ready():
+	var rng = RandomNumberGenerator.new()
+	$Polygon2D.color.h += rng.randf_range(-0.025, 0.025)
+	$Polygon2D.color.s += rng.randf_range(-0.025, 0.025)
+	$Polygon2D.color.v += rng.randf_range(-0.025, 0.025)
+
 func _on_area_entered(area : Area2D):
 	if area.is_in_group("mob"):
 		area.on_hit(2000)
