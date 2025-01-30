ee5a8b9240158d1b616afd2d848ff8c62f1df3ef
Jason Abbott <woebane@gmail.com>
2025-01-30 12:22:19 -0800

    Updated rock graphics

--- Scripts/rock.gd
+++ Scripts/rock.gd
@@ -2,9 +2,15 @@ extends Area2D
 
 func _ready():
 	var rng = RandomNumberGenerator.new()
-	$Polygon2D.color.h += rng.randf_range(-0.025, 0.025)
-	$Polygon2D.color.s += rng.randf_range(-0.025, 0.025)
-	$Polygon2D.color.v += rng.randf_range(-0.025, 0.025)
+	var polygon : Polygon2D
+	match rng.randi() % 3:
+		0: polygon = $VisualShape1
+		1: polygon = $VisualShape2
+		2: polygon = $VisualShape3
+	polygon.color.h += rng.randf_range(-0.025, 0.025)
+	polygon.color.s += rng.randf_range(-0.025, 0.025)
+	polygon.color.v += rng.randf_range(-0.025, 0.025)
+	polygon.visible = true
 
 func _on_area_entered(area : Area2D):
 	if area.is_in_group("mob"):
